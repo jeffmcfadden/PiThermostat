aa544f2363f21b4b2e774ec82978dbd2d64d4951
Ben McFadden <ben@ministrycentered.com>
2016-06-26 14:36:43 -0700

    Update update_temp route

--- update_temp.rb
+++ update_temp.rb
@@ -12,5 +12,5 @@ cRaw = s.split( "\n" )[1].split( "t=" )[1].to_i
 @c = cRaw / 1000.0
 @f = (@c * (9.0/5.0) ) + 32
 
-cmd = "curl -silent -X PATCH --data \"thermostat[current_temperature]=#{@f}\" http://127.0.0.1/thermostats/1"
+cmd = "curl -silent -X PATCH --data \"thermostat[current_temperature]=#{@f}\" http://127.0.0.1/thermostat"
 system( cmd )
